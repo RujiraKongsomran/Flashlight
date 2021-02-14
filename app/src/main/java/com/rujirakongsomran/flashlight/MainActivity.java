3ed67d6ea56139f6bc433baf1856f9d1623ed206
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-02-14 22:24:13 +0700

    Enable ViewBinding and Write JAVA Code finish

--- app/src/main/java/com/rujirakongsomran/flashlight/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/flashlight/MainActivity.java
@@ -2,13 +2,90 @@ package com.rujirakongsomran.flashlight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.rujirakongsomran.flashlight.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    boolean state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                runFlashlight();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(MainActivity.this, "Camera permission required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
     }
+
+    private void runFlashlight() {
+
+        binding.ibFlashlight.setOnClickListener(ibFlashlightOnClickListener);
+    }
+
+    // Listener
+    View.OnClickListener ibFlashlightOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            if (!state) {
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        cameraManager.setTorchMode(cameraId, true);
+                        state = true;
+                        binding.ibFlashlight.setImageResource(R.drawable.torch_on);
+                    }
+
+                } catch (CameraAccessException e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        cameraManager.setTorchMode(cameraId, false);
+                        state = false;
+                        binding.ibFlashlight.setImageResource(R.drawable.torch_off);
+                    }
+
+                } catch (CameraAccessException e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
 }
